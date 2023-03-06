be621bc53e3009fddadd19c250ad2a5046ec0199
Makana-Grey <makanagreyx@gmail.com>
2023-03-06 19:53:02 +0300

    fix api

--- FloppyCatsOnlineAPI/src/server.js
+++ FloppyCatsOnlineAPI/src/server.js
@@ -1,18 +1,11 @@
 import Koa from 'koa'
 import { koaBody } from 'koa-body'
-import serverless from 'serverless-http'
-import Router from 'koa-router'
 
 import routes from './routes.js'
 
 const app = new Koa()
-const router = new Router()
 
 app.use(koaBody())
+app.use(routes)
 
-router.use('/.netlify/functions/api', routes)
-
-app.use(router.routes())
-
-export const handler = serverless(app)
 export default app
